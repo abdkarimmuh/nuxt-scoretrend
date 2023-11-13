ccda1bdc305de2c3f66a6c4889ca276d00bde9c1
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2023-11-14 00:10:05 +0700

    fix structure directory

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Layout</p>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Index</h1>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "default",
+});
+</script>
+
+<style>
+</style>
